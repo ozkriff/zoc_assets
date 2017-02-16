bec029392ab95373e711f9d3c2dcbcb8452a8766
Andrey Lesnikov <ozkriff@gmail.com>
2017-02-16 09:36:38 +0300

    Removed infile vim settings

--- shader/pre_gl.glsl
+++ shader/pre_gl.glsl
@@ -1,3 +1,1 @@
 #version 120
-
-// vim: set tabstop=4 shiftwidth=4 softtabstop=4 expandtab:
--- shader/pre_gles.glsl
+++ shader/pre_gles.glsl
@@ -2,5 +2,3 @@
 
 precision mediump float;
 precision mediump int;
-
-// vim: set tabstop=4 shiftwidth=4 softtabstop=4 expandtab:
